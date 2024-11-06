5a643dcf37238d7636b7607bdb46e4010ccfc2b7
PuggyPotato <qingyaurou@gmail.com>
2024-11-06 19:34:44 +0800

    Fixed A Little Bug Related To Width of Input

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 
 
 const App = () =>{
-  const [OriginalURL,setOriginalURL] = useState("hello")
+  const [OriginalURL,setOriginalURL] = useState("")
   const [ShortenedURL,setShortenedURL] = useState("")
 
   function changeOriginalURL(event){
@@ -30,14 +30,13 @@ const App = () =>{
     })
   }
 
-  console.log(ShortenedURL)
   return(
     <>
       <div className="h-screen flex justify-center items-center font-mono">
         <form onSubmit={handleSubmit} className="border-2 h-[50%] w-[50%] flex justify-center relative">
           <h1 className="text-3xl absolute top-[5%]">URL Shortener</h1>
-          <label className="absolute top-[25%] left-[25%] text-xl">Insert Your URL Here:
-            <input value={OriginalURL} onChange={changeOriginalURL} className="border-2 bg-blue-400 top-[-15%] absolute left-[105%] text-black p-1"></input>
+          <label className="absolute top-[25%] left-[0%] text-xl relative">Insert Your URL Here:
+            <input value={OriginalURL} onChange={changeOriginalURL} className="border-2 bg-blue-400 top-[0%] absolute left-[1%] text-black p-1 w-[50%] relative text-center" placeholder="https://www.google.com"></input>
             </label>
           <a href={`https://potato-b015.onrender.com/${ShortenedURL}`} className="absolute top-[45%]"><p>Your Shortened URL is {ShortenedURL}</p></a>
           <button type="submit" className="absolute top-[60%] border-2 bg-blue-300 p-2 w-[20%]">Shorten</button>
